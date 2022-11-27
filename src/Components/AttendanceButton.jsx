78e581e77dd7d52c81bbd4531ce755519ddda498
joonhyuk.han <virq@naver.com>
2022-11-27 21:05:18 +0900

    feat: 출석체크 버튼 비활성화#20

--- src/Components/AttendanceButton.jsx
+++ src/Components/AttendanceButton.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
@@ -7,10 +8,14 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Alert from "@mui/material/Alert";
+import useStore from "../store.js";
 
 const AttendanceButton = () => {
   const [open, setOpen] = useState(false);
+  const [buttonStatus, setButton] = useState(true);
+  const [buttonLetter, setLetter] = useState("출석체크");
   const [isSameWithTodayWord, setIsSameWithTodayWord] = useState(true);
+  const { _intraId } = useStore((state) => state);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -23,52 +28,82 @@ const AttendanceButton = () => {
   // TODO 서버와 통신해서 오늘의 단어 정답 여부 처리하기
   // TODO 오답일 경우 setIsSameWithTodayWord(false) 처리하기
   const handleSubmit = async (event) => {
-    const inputValue = document.getElementById("todayWord").value;
-    console.log(inputValue);
+    event.preventDefault();
+    const inputValue = event.target.todayWord.value;
+
     try {
       const response = await axios.post(
         "http://10.19.202.231:3000/attendance/userAttendance",
         {
-          intraId: "joonhan",
+          intraId: _intraId,
           todayWord: inputValue,
         }
       );
-      console.log(response);
+      if (response.status === 200) {
+        setOpen(false);
+      } else if (response.data.statusAttendance === 2) {
+        setIsSameWithTodayWord(false);
+      }
     } catch (error) {
       console.log(error);
     }
-    setOpen(false);
   };
+
+  const buttonChecker = async () => {
+    try {
+      const response = await axios.get(
+        "http://10.19.202.231:3000/attendance/mgo/buttonStatus"
+      );
+      setButton(response.data);
+
+      if (response.data === 1) setLetter("출석가능 시간이 아닙니다");
+      else if (response.data === 2) setLetter("이미 출석체크를 완료했습니다");
+      else if (response.data === 3)
+        setLetter("오늘의 단어가 아직 설정되지 않았습니다");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    buttonChecker();
+  }, []);
 
   return (
     <>
       <Button
-        onClick={handleClickOpen}
+        disabled={buttonStatus !== 0 ? true : false}
+        onClick={() => {
+          handleClickOpen();
+          buttonChecker();
+        }}
         variant="contained"
         color="success"
+        align="center"
         sx={{ mt: 3, width: 1 / 2 }}
       >
-        출석체크
+        {buttonLetter}
       </Button>
       <Dialog open={open} onClose={handleClose}>
         {!isSameWithTodayWord && (
           <Alert severity="error">오늘의 단어와 일치하지 않습니다.</Alert>
         )}
         <DialogTitle>오늘의 단어를 입력해주세요.</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="todayWord"
-            label="오늘의 단어"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>취소</Button>
-          <Button onClick={handleSubmit}>제출</Button>
-        </DialogActions>
+        <Box component="form" onSubmit={handleSubmit}>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="todayWord"
+              label="오늘의 단어"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>취소</Button>
+            <Button type="submit">제출</Button>
+          </DialogActions>
+        </Box>
       </Dialog>
     </>
   );
