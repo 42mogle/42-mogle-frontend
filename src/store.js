128e4b619cf2f8276cf066b3ef5dd35e4ae84dbd
joonhyuk.han <virq@naver.com>
2022-11-27 21:58:23 +0900

    feat: AWS 서버 변수 추가#27

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import create from "zustand"; // create로 zustand를 불러옵니다.
 
 const useStore = create((set) => ({
   _intraId: "",
-  setIntraId: (intraId) => set({ _intraId: intraId })
+  _server: "13.124.3.170:3000",
+  setIntraId: (intraId) => set({ _intraId: intraId }),
 }));
 
 export default useStore;
