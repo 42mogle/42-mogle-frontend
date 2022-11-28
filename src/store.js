f02d454462d722fb950a1b5cd19bd6a883236218
joonhyuk.han <virq@naver.com>
2022-11-28 16:16:23 +0900

    hotfix: 포트번호 3000번 추가 #38

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ const useStore = create(
     (set) => ({
       _oauth:
         "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-ffa1eb7dfe8ca1260f9d27ba33051536d23c76cd1ab09f489cb233c7e8e5e065&redirect_uri=https%3A%2F%2F42mogle.com%2Fauth&response_type=code",
-      _server: "https://42mogle.com",
+      _server: "https://42mogle.com:3000",
       _intraId: "",
       setIntraId: (intraId) => set({ _intraId: intraId }),
       _isAttended: false,
