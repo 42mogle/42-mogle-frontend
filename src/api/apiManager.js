6d13d708d90a9fbc8b3bd46ac71381b34beb02b7
joonhyuk.han <virq@naver.com>
2022-12-05 13:19:11 +0900

    refactor: Modify apiManager #43

--- src/api/apiManager.js
+++ src/api/apiManager.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const apiManager = () => {
-  return axios.create({
-    baseURL: `https://${process.env.REACT_APP_AWS_BACKEND_SERVER}`,
-    headers: {
-			authorization: `Bearer ${localStorage.get("accessToken")}`
-    },
-  });
-};
+const apiManager = axios.create({
+  baseURL: `https://${process.env.REACT_APP_AWS_BACKEND_SERVER}`,
+  headers: {
+    authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+});
 
 export default apiManager;
