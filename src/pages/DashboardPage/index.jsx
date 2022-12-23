a17ca564c04328ae7fe3398f7c3e466222b99d78
joonhyuk.han <virq@naver.com>
2022-12-23 12:02:19 +0900

    style: 연도, 월, 조회 버튼 그리드 적용 - 조회 버튼은 다시 적용해야함

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -5,6 +5,7 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 const START_YEAR = 2022;
 const current_year = new Date().getFullYear();
@@ -28,40 +29,93 @@ function DashboardPage() {
   const handleMonth = (event) => {
     setSelectMonth(event.target.value);
   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="year-label">연도</InputLabel>
-        <Select
-          labelId="year"
-          id="year"
-          value={selectYear}
-          label="Year"
-          onChange={handleYear}
+      <Grid container spacing={3}>
+        <Grid
+          container
+          item
+          xs={12}
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
         >
-          {years.map((year) => (
-            <MenuItem key={year} value={year}>
-              {year}
-            </MenuItem>
-          ))}
-        </Select>
+          <Grid item xs={2}>
+            <FormControl>
+              <InputLabel id="year-label">연도</InputLabel>
+              <Select
+                labelId="year"
+                id="year"
+                value={selectYear}
+                label="Year"
+                onChange={handleYear}
+              >
+                {years.map((year) => (
+                  <MenuItem key={year} value={year}>
+                    {year}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={2}>
+            <FormControl>
+              <InputLabel id="month-label">월</InputLabel>
+              <Select
+                labelId="month"
+                id="month"
+                value={selectMonth}
+                label="month"
+                onChange={handleMonth}
+              >
+                {months.map((month) => (
+                  <MenuItem key={month} value={month}>
+                    {month}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={2}>
+            <Button variant="outlined">
+              조회
+            </Button>
+          </Grid>
+        </Grid>
 
-        <InputLabel id="month-label">월</InputLabel>
-        <Select
-          labelId="month"
-          id="month"
-          value={selectMonth}
-          label="month"
-          onChange={handleMonth}
-        >
-          {months.map((month) => (
-            <MenuItem key={month} value={month}>
-              {month}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
+        {/* Recent Deposits */}
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
+        {/* Recent Orders */}
+        <Grid item xs={12}>
+          <Paper
+            sx={{ p: 2, display: "flex", flexDirection: "column" }}
+          ></Paper>
+        </Grid>
+      </Grid>
     </>
   );
 }
