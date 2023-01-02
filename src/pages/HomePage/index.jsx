b206463d4c4fed7f8f518586985784683d8a827d
joonhyuk.han <virq@naver.com>
2023-01-02 11:33:42 +0900

    feat: 출석체크 페이지에서 토큰 만료 시 로그인 다시 요청하는 팝업창 생성

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import apiManager from "../../api/apiManager";
+import LoginChecker from "../../components/LoginChecker";
 import AttendanceTable from "./AttendanceTable";
 import AttendanceButton from "./AttendanceButton";
 import SetTodayWordButton from "./SetTodayWordButton";
 import UserProfile from "./UserProfile";
 import TodayDate from "./TodayDate";
 import AttendanceSummary from "./AttendanceSummary";
-import Link from "@mui/material/Link";
 import useStore from "../../store.js";
 
 function Home() {
@@ -32,13 +32,15 @@ function Home() {
 
   return (
     <>
-      <UserProfile intraId={_intraId} photoUrl={_photoUrl} />
-      <TodayDate />
-      <AttendanceTable>
-        <AttendanceSummary />
-      </AttendanceTable>
-      <AttendanceButton />
-      {isOperator && <SetTodayWordButton />}
+      <LoginChecker>
+        <UserProfile intraId={_intraId} photoUrl={_photoUrl} />
+        <TodayDate />
+        <AttendanceTable>
+          <AttendanceSummary />
+        </AttendanceTable>
+        <AttendanceButton />
+        {isOperator && <SetTodayWordButton />}
+      </LoginChecker>
     </>
   );
 }
