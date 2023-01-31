32a35a37a8488067802872a70496ce72620a797f
joonhan <joonhan@student.42seoul.kr>
2023-02-01 08:03:55 +0900

    feat: 대시보드 페이지 접근 권한 추가 #140
    - 오퍼레이터가 아닌 유저가 접근할 경우 이전 페이지로 이동
    - 오퍼레이터이지만 토큰의 유효기간이 만료되면 팝업창 표시

--- src/components/DesktopLayout.jsx
+++ src/components/DesktopLayout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OperatorChecker from "./OperatorChecker";
 import { Outlet } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
@@ -22,92 +23,95 @@ const drawerWidth = 240;
 
 function DesktopLayout(props) {
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-          bgcolor: "white",
-          color: "black",
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            🌻 42 Morning Glory
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <>
+      <OperatorChecker />
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            bgcolor: "white",
+            color: "black",
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              🌻 42 Morning Glory
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: {
+        <Drawer
+          variant="permanent"
+          sx={{
             width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: "auto" }}>
-          <List>
-            <ListItem>
-              <CardHeader
-                avatar={
-                  <Avatar
-                    src="https://i.ytimg.com/vi/AwrFPJk_BGU/maxresdefault.jpg"
-                    sx={{ bgcolor: "red[500]", boxShadow: "3" }}
-                    aria-label="profile"
-                  />
-                }
-                title="Intra Id"
-                subheader="Operator"
-              />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <ListItemButton href="/dashboard">
-                <ListItemIcon>
-                  <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText primary="시즌별 통계"/>
-              </ListItemButton>
-            </ListItem>
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              <ListItem>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      src="https://i.ytimg.com/vi/AwrFPJk_BGU/maxresdefault.jpg"
+                      sx={{ bgcolor: "red[500]", boxShadow: "3" }}
+                      aria-label="profile"
+                    />
+                  }
+                  title="Intra Id"
+                  subheader="Operator"
+                />
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemButton href="/dashboard">
+                  <ListItemIcon>
+                    <DashboardIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="시즌별 통계" />
+                </ListItemButton>
+              </ListItem>
 
-            <ListItem>
-              <ListItemButton href="/dashboard/setting">
-                <ListItemIcon>
-                  <SettingsIcon />
-                </ListItemIcon>
-                <ListItemText primary="설정"/>
-              </ListItemButton>
-            </ListItem>
-          </List>
+              <ListItem>
+                <ListItemButton href="/dashboard/setting">
+                  <ListItemIcon>
+                    <SettingsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="설정" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+            {/* Children components */}
+            <Outlet />
+            {/* Children components */}
+          </Container>
         </Box>
-      </Drawer>
-
-      <Box
-        component="main"
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === "light"
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
-          flexGrow: 1,
-          height: "100vh",
-          overflow: "auto",
-        }}
-      >
-        <Toolbar />
-        <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-          {/* Children components */}
-          <Outlet />
-          {/* Children components */}
-        </Container>
       </Box>
-    </Box>
+    </>
   );
 }
 
