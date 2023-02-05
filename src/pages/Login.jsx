ed92258f089a20bc1072bddea9a1043300ae435c
joonhan <joonhan@student.42seoul.kr>
2023-02-05 14:12:48 +0900

    fix: jwt 토큰이 유효한 경우 로그인 페이지로 이동하지 못하도록 설정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, redirect } from "react-router-dom";
 import apiManager from "../api/apiManager.js";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -9,6 +9,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Alert from "@mui/material/Alert";
 import Snackbar from "@mui/material/Snackbar";
+import jwt_decode from "jwt-decode";
 import useStore from "../store.js";
 
 const Login = () => {
@@ -18,8 +19,6 @@ const Login = () => {
   const [isErrorOccurred, setisErrorOccurred] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [signupSnackbarOpen, setSignupSnackbarOpen] = useState(false);
-
-  console.log(state);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -34,6 +33,29 @@ const Login = () => {
       setSignupSnackbarOpen(true);
     }
     setIsClickedPasswordReset(false);
+  }, []);
+
+  const isTokenExpired = (token) => {
+    const decodedToken = jwt_decode(token);
+    const expirationDate = decodedToken.exp * 1000;
+    const currentTimestamp = Date.now();
+    return (expirationDate < currentTimestamp);
+  }
+
+  const checkLoginStatus = () => {
+    const jwtToken = localStorage.getItem("accessToken");
+    if (jwtToken === null)
+      return ;
+    if (isTokenExpired(jwtToken))
+    {
+      localStorage.removeItem("accessToken");
+      return ;
+    }
+    navigate(-1);
+  }
+
+  useEffect(() => {
+    checkLoginStatus();
   }, []);
 
   const handleLoginSubmit = async (event) => {
