d2b1cffb9f47d378fedc688cdfc45dd9c1a9a0ac
joonhan <joonhan@student.42seoul.kr>
2023-02-08 11:10:25 +0900

    feat: 오퍼레이터에게 오늘의 단어 표시 #148
    - 오늘의 단어 설정 시 공백만 들어오는 경우는 오늘의 단어가 설정되지 않은 것으로 판단

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,11 +8,46 @@ import UserProfile from "./UserProfile";
 import TodayDate from "./TodayDate";
 import AttendanceSummary from "./AttendanceSummary";
 import useStore from "../../store.js";
+import TodayWord from "./TodayWord";
+const HTTP_STATUS = require("http-status");
 
 function Home() {
   const { _intraId, _photoUrl, setPhotoUrl } = useStore((state) => state);
   // TODO: Home 컴포넌트가 여러 번 렌더링 되는 문제가 있음
   const [isOperator, setIsOperator] = useState(false);
+  const [todayWord, setTodayWord] = useState({
+    word: "",
+    textColor: "",
+    isTodayWordSet: false,
+  });
+
+  const getTodayWord = async () => {
+    try {
+      const response = await apiManager.get("/operator/today-word");
+      if (response.status === HTTP_STATUS.OK) {
+        if (response.data.length === 0) {
+          setTodayWord({
+            word: "",
+            textColor: "warning.main",
+            isTodayWordSet: false,
+          });
+        } else {
+          setTodayWord({
+            word: response.data,
+            textColor: "success.main",
+            isTodayWordSet: true,
+          });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setTodayWord({
+        word: "오류가 발생했습니다.",
+        textColor: "error.main",
+        isTodayWordSet: false,
+      });
+    }
+  };
 
   const getUserInfo = async () => {
     try {
@@ -30,6 +65,10 @@ function Home() {
     getUserInfo();
   }, []);
 
+  useEffect(() => {
+    getTodayWord();
+  }, []);
+
   return (
     <>
       <LoginChecker />
@@ -39,11 +78,14 @@ function Home() {
         isOperator={isOperator}
       />
       <TodayDate />
+      {isOperator && <TodayWord todayWord={todayWord} />}
       <AttendanceTable>
         <AttendanceSummary />
       </AttendanceTable>
       <AttendanceButton />
-      {isOperator && <SetTodayWordButton />}
+      {isOperator && (
+        <SetTodayWordButton todayWord={todayWord} setTodayWord={setTodayWord} />
+      )}
     </>
   );
 }
