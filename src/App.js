3b85fe0f8d36e97fd3c81885734310478c9d4331
joonhan <joonhan@student.42seoul.kr>
2023-02-17 08:03:59 +0900

    feat: Add NotFoundPage and ClosedPage#150

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import getServerStatus from "utils/getServerStatus";
 import MobileLayout from "./components/MobileLayout";
 import DesktopLayout from "./components/DesktopLayout";
 import Login from "./pages/Login";
@@ -8,25 +9,28 @@ import Auth from "./pages/Auth";
 import DashboardPage from "./pages/DashboardPage";
 import SettingPage from "./pages/SettingPage";
 import ClosedPage from "./pages/ClosedPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
+  if (getServerStatus() === false) {
+    return <ClosedPage />;
+  }
+
   return (
-    <>
-      <Routes>
-        <Route path="/closed" element={<ClosedPage />} />
-        <Route element={<MobileLayout />}>
-          <Route path="/" element={<Login />} />
-          <Route path="/reset-password" element={<Signup />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/auth" element={<Auth />} />
-        </Route>
-        <Route element={<DesktopLayout />}>
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/dashboard/setting" element={<SettingPage />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<MobileLayout />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/reset-password" element={<Signup />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/auth" element={<Auth />} />
+      </Route>
+      <Route element={<DesktopLayout />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard/setting" element={<SettingPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
