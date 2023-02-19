797c17fe73a112897d73624d45f1639df2d35736
SeungWoonSong <susong@student.42seoul.kr>
2023-02-19 16:27:57 +0900

    unused console.log remove

--- src/utils/getServerStatus.jsx
+++ src/utils/getServerStatus.jsx
@@ -4,15 +4,11 @@ const HTTP_STATUS = require("http-status");
 const getServerStatus = async () => {
 	try {
 		const response = await apiManager.get("/server-status");
-		console.log("response: ", response);
 		if (response.status === HTTP_STATUS.OK) {
-			console.log("true");
 			return (true);
 		}
-		console.log("false up");
 		return (false);
 	} catch(error) {
-		console.log("false down");
 		console.error(error);
 		return (false);
 	}
