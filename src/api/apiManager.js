cf3a0469262bffeb43da42116e0f37a44010425e
SeungWoon Song <38645951+SeungWoonSong@users.noreply.github.com>
2023-03-07 09:07:47 +0900

    fix : timeout 1.5 to 3

--- src/api/apiManager.js
+++ src/api/apiManager.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiManager = axios.create({
   baseURL: `https://${process.env.REACT_APP_AWS_BACKEND_SERVER}`,
-  timeout: 1500,
+  timeout: 3000,
 });
 
 apiManager.interceptors.request.use(
