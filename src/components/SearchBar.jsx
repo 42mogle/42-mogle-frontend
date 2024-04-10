13409c5b00d592b1849d75c5057a5bcaaa0adc61
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-04-10 11:05:33 +0900

    ✨ feat: Add searchQuery, onSubmit props in SearchBar #164
    - 초기화 버튼 추가
    - onSubmit 을 상위 컴포넌트에서 받아서 실행

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,27 +2,38 @@ import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
 
 function SearchBar(props) {
-  const { setSearchQuery } = props;
+  const { searchQuery, setSearchQuery, onSubmit } = props;
 
   const handleSearchQuery = (event) => {
     event.preventDefault();
     setSearchQuery(event.target.searchBar.value);
+    onSubmit();
+  };
+
+  const handleResetButton = () => {
+    setSearchQuery("");
   };
 
   return (
-    <Box sx={{ mt: 2, mb: 2 }}component="form" onSubmit={handleSearchQuery}>
+    <Box sx={{ mt: 2, mb: 2 }} component="form" onSubmit={handleSearchQuery}>
       <TextField
         id="searchBar"
         label="Intra ID"
         variant="outlined"
         placeholder="Intra ID 를 입력하세요."
         size="small"
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)}
       />
       <IconButton type="submit" aria-label="search">
         <SearchIcon />
       </IconButton>
+      <Button variant="text" onClick={handleResetButton}>
+        초기화
+      </Button>
     </Box>
   );
 }
